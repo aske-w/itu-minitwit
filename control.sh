207bafe02068e7a13d6065ddea438c7938fd34e7
aske-w <askw@itu.dk>
2022-02-01 13:49:33 +0100

    shellcheck refactoring

--- control.sh
+++ control.sh
@@ -1,6 +1,7 @@
+#! /bin/bash
 DB_LOCATION="./tmp/minitwit.db"
 
-if [ $1 = "init" ]; then
+if [ "$1" = "init" ]; then
 
     if [ -f DB_LOCATION ]; then 
         echo "Database already exists."
@@ -8,15 +9,15 @@ if [ $1 = "init" ]; then
     fi
     echo "Putting a database to ${DB_LOCATION}..." 
     python3 -c"from minitwit import init_db;init_db()"
-elif [ $1 = "start" ]; then
+elif [ "$1" = "start" ]; then
     echo "Starting minitwit..."
-    nohup `which python3` minitwit.py > ./tmp/out.log 2>&1 &
-elif [ $1 = "stop" ]; then
+    nohup "which python3" minitwit.py > ./tmp/out.log 2>&1 &
+elif [ "$1" = "stop" ]; then
     echo "Stopping minitwit..."
     pkill -f minitwit
-elif [ $1 = "inspectdb" ]; then
+elif [ "$1" = "inspectdb" ]; then
     ./flag_tool -i | less
-elif [ $1 = "flag" ]; then
+elif [ "$1" = "flag" ]; then
     ./flag_tool "$@"
 else
   echo "I do not know this command..."
