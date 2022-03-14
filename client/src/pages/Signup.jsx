899c34c53e297bb6c56b44e4c644d7e6c2de59de
Christian Gerdes <christian@feedr.dk>
2022-03-14 15:59:59 +0100

    Added sign up page to client
    Co-Authored-By: larsdjursner <40217101+larsdjursner@users.noreply.github.com>
    Co-Authored-By: Tobias Svendsen <62613136+tobiassvendsen@users.noreply.github.com>

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from '../api'
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    const [form, setForm] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+
+    const handleChange = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        api.post("/signup", form)
+    }
+
+    return (
+        <div>
+            <h2>Sign up</h2>
+
+            <form action="POST" onSubmit={handleSubmit}>
+                <dl>
+                    <dt>Username:</dt>
+                    <dd>
+                        <input type="text" name="username" size="30" onChange={handleChange}/>
+                    </dd>
+
+                    <dt>E-Mail:</dt>
+                    <dd>
+                        <input type="text" name="email" size="30" onChange={handleChange}/>
+                    </dd>
+
+                    <dt>Password:</dt>
+                    <dd>
+                        <input type="password" name="password" size="30" onChange={handleChange}/>
+                    </dd>
+                </dl>
+
+                <div className="actions">
+                    <input type="submit" value="Sign Up"/>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default Signup
