070e1551d6b6d8c8c0024e18b403069251fe1e1e
Christian Gerdes <christian@feedr.dk>
2022-03-14 16:00:32 +0100

    Added sign in page with auth logic and http interceptors
    Co-Authored-By: larsdjursner <40217101+larsdjursner@users.noreply.github.com>
    Co-Authored-By: Tobias Svendsen <62613136+tobiassvendsen@users.noreply.github.com>

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import api from "../api"
+import { login } from '../reducers/auth'
+import { useDispatch } from 'react-redux'
 
 const Signin = () => {
-  return (
-    <div>Signin</div>
-  )
+	const [form, setForm] = useState({
+        username: "",
+        password: "",
+    })
+
+	const dispatch = useDispatch()
+
+    const handleChange = (event) => {
+        setForm({...form, [event.target.name]: event.target.value})
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+		api.post("signin", form).then(response => {
+			dispatch(login(response.data))
+        })
+    }
+
+  	return (
+		<div>
+			<h2>Sign in</h2>
+			<form onSubmit={handleSubmit}>
+				<dl>
+					<dt>Username:</dt>
+					<dd><input type="text" name="username" onChange={handleChange}/></dd>
+
+					<dt>Password:</dt>
+					<dd><input type="password" name="password" onChange={handleChange}/></dd>
+				</dl>
+
+				<div className="actions">
+					<input type="submit" value="Sign In"/>
+				</div>
+			</form>
+		</div>
+  	)
 }
 
 export default Signin
