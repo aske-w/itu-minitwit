2c6634a99c42d0cca7f329c04965944aa8df4db5
Christian Gerdes <christian@feedr.dk>
2022-03-14 15:58:36 +0100

    Updated API to enable sign up, sign in, and timeline with JWT
    Co-Authored-By: larsdjursner <40217101+larsdjursner@users.noreply.github.com>
    Co-Authored-By: Tobias Svendsen <62613136+tobiassvendsen@users.noreply.github.com>

--- server/go.mod
+++ server/go.mod
@@ -16,21 +16,26 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/eknkc/amber v0.0.0-20171010120322-cdade1c07385 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/flosch/pongo2/v4 v4.0.2 // indirect
+	github.com/go-redis/redis/v8 v8.11.4 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gobwas/httphead v0.1.0 // indirect
 	github.com/gobwas/pool v0.2.1 // indirect
 	github.com/gobwas/ws v1.1.0 // indirect
+	github.com/goccy/go-json v0.9.4 // indirect
+	github.com/golang-jwt/jwt/v4 v4.3.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/iris-contrib/blackfriday v2.0.0+incompatible // indirect
 	github.com/iris-contrib/go.uuid v2.0.0+incompatible // indirect
 	github.com/iris-contrib/jade v1.1.4 // indirect
+	github.com/iris-contrib/middleware/jwt v0.0.0-20220301201128-27fa0f6a7d7e // indirect
 	github.com/iris-contrib/pongo2 v0.0.1 // indirect
 	github.com/iris-contrib/schema v0.0.6 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -38,14 +43,15 @@ require (
 	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/kataras/blocks v0.0.4 // indirect
+	github.com/kataras/blocks v0.0.5 // indirect
 	github.com/kataras/golog v0.1.7 // indirect
-	github.com/kataras/iris/v12 v12.2.0-alpha5.0.20220122232131-4899fe95f47d // indirect
-	github.com/kataras/neffos v0.0.18 // indirect
+	github.com/kataras/iris/v12 v12.2.0-alpha9 // indirect
+	github.com/kataras/jwt v0.1.2 // indirect
+	github.com/kataras/neffos v0.0.19 // indirect
 	github.com/kataras/pio v0.0.10 // indirect
 	github.com/kataras/sitemap v0.0.5 // indirect
 	github.com/kataras/tunnel v0.0.3 // indirect
-	github.com/klauspost/compress v1.14.2 // indirect
+	github.com/klauspost/compress v1.14.4 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-sqlite3 v1.14.11 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
@@ -54,7 +60,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/nats-io/jwt v0.3.2 // indirect
-	github.com/nats-io/nats.go v1.13.1-0.20211122170419-d7c1d78a50fc // indirect
+	github.com/nats-io/nats.go v1.13.1-0.20220121202836-972a071d373d // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
@@ -71,14 +77,14 @@ require (
 	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	github.com/yosssi/ace v0.0.5 // indirect
-	golang.org/x/crypto v0.0.0-20220208233918-bba287dce954 // indirect
-	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11 // indirect
+	golang.org/x/time v0.0.0-20220224211638-0e9765cccd65 // indirect
 	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
-	gopkg.in/ini.v1 v1.66.3 // indirect
+	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gorm.io/driver/sqlite v1.3.1 // indirect
 	gorm.io/gorm v1.23.1 // indirect
--- server/main.go
+++ server/main.go
@@ -3,65 +3,63 @@ package main
 import (
 	"aske-w/itu-minitwit/database"
 	"aske-w/itu-minitwit/environment"
-	"aske-w/itu-minitwit/middleware"
 	"aske-w/itu-minitwit/models"
 	"aske-w/itu-minitwit/services"
 	"aske-w/itu-minitwit/web/controllers"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/kataras/iris/v12"
-	"github.com/kataras/iris/v12/middleware/logger"
-	"github.com/kataras/iris/v12/middleware/recover"
-	"github.com/kataras/iris/v12/mvc"
-	"github.com/kataras/iris/v12/sessions"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/kataras/iris/v12/middleware/jwt"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
-func main() {
+var (
+	sigKey = []byte("secret")
+	encKey = []byte("GCM_AES_256_secret_shared_key_32")
+)
 
-	app := iris.New()
+type UserClaims struct {
+	Id       uint   `json:"id"`
+	Username string `json:"username"`
+}
+
+func main() {
+	app := iris.Default()
+
 	// app.Logger().SetLevel("debug") // more logging
-	app.Favicon("./web/public/favicon.ico")
+	// app.Favicon("./web/public/favicon.ico")
 	// Load env's
 	environment.InitEnv()
 
-	app.Use(logger.New())  // logs request
-	app.Use(recover.New()) // handles panics (shows 404)
-
-	crs := func(ctx iris.Context) {
-		ctx.Header("Access-Control-Allow-Origin", "*")
-
-		ctx.Next()
-	}
-
-	app.UseRouter(crs)
+	// app.Use(logger.New())  // logs request
+	// app.Use(recover.New()) // handles panics (shows 404)
 
 	// Register middleware
-	app.Use(middleware.InitMiddleware)
+	// app.Use(middleware.InitMiddleware)
 
 	// Configure sessions manager.
-	sess := sessions.New(sessions.Config{
-		Cookie:                      "itu-minitwit-cookie",
-		AllowReclaim:                true,
-		DisableSubdomainPersistence: true,
-	})
-	app.Use(sess.Handler())
+	// sess := sessions.New(sessions.Config{
+	// 	Cookie:                      "itu-minitwit-cookie",
+	// 	AllowReclaim:                true,
+	// 	DisableSubdomainPersistence: true,
+	// })
+	// app.Use(sess.Handler())
 
 	// Add html files
-	tmpl := iris.HTML("./web/views", ".html").
-		Layout("shared/layout.html").
-		Reload(true)
-	app.RegisterView(tmpl)
-	app.HandleDir("/public", "./web/public")
+	// tmpl := iris.HTML("./web/views", ".html").
+	// 	Layout("shared/layout.html").
+	// 	Reload(true)
+	// app.RegisterView(tmpl)
+	// app.HandleDir("/public", "./web/public")
 
-	// Register default error view
-	app.OnAnyErrorCode(func(ctx iris.Context) {
-		ctx.ViewData("Message", ctx.Values().GetStringDefault("Message", "Error occured"))
-		ctx.View("shared/error.html")
-	})
+	// // Register default error view
+	// app.OnAnyErrorCode(func(ctx iris.Context) {
+	// 	ctx.ViewData("Message", ctx.Values().GetStringDefault("Message", "Error occured"))
+	// 	ctx.View("shared/error.html")
+	// })
 
 	db, err := database.ConnectMySql()
 	if err != nil {
@@ -69,76 +67,243 @@ func main() {
 	}
 	userService := services.NewUserService(db)
 	authService := services.NewAuthService(db)
-	timelineService := services.NewTimelineService(db)
-	messageService := services.NewMessageService(db)
+	// timelineService := services.NewTimelineService(db)
+	// messageService := services.NewMessageService(db)
 
-	// I cant figure out how to have global DI, when using MVC pattern?
-	index := mvc.New(app.Party("/"))
-	index.Register(timelineService)
-	index.Register(messageService)
-	index.Register(userService)
-	index.Handle(new(controllers.IndexController))
+	// // I cant figure out how to have global DI, when using MVC pattern?
+	// index := mvc.New(app.Party("/"))
+	// index.Register(timelineService)
+	// index.Register(messageService)
+	// index.Register(userService)
+	// index.Handle(new(controllers.IndexController))
 
-	auth := mvc.New(app.Party("/"))
-	auth.Register(userService)
-	auth.Register(authService)
-	auth.Handle(new(controllers.AuthController))
+	// auth := mvc.New(app.Party("/"))
+	// auth.Register(userService)
+	// auth.Register(authService)
+	// auth.Handle(new(controllers.AuthController))
 
-	// Setup prometheus for monitoring
-	var count int64 = 0
-	var avgFollowers float64 = 0
-	usersCount := promauto.NewGauge(prometheus.GaugeOpts{
-		Subsystem: "minitwit",
-		Name:      "total_users_count",
-		Help:      "The total amount of users in the database",
+	// // Setup prometheus for monitoring
+	// var count int64 = 0
+	// var avgFollowers float64 = 0
+	// usersCount := promauto.NewGauge(prometheus.GaugeOpts{
+	// 	Subsystem: "minitwit",
+	// 	Name:      "total_users_count",
+	// 	Help:      "The total amount of users in the database",
+	// })
+	// avgFollowersCount := promauto.NewGauge(prometheus.GaugeOpts{
+	// 	Subsystem: "minitwit",
+	// 	Name:      "average_followers_count",
+	// 	Help:      "The total amount of users in the database",
+	// })
+	// //run non-middleware metrics data collection for in separate thread.
+	// // middleware data is collected in ./middleware/prometheusMiddleware.go
+	// go func() {
+	// 	for {
+	// 		db.Model(&models.User{}).Count(&count)
+	// 		db.Raw("select ((select count(follower_id) from followers) / (select count(*) from users));").Scan(&avgFollowers)
+	// 		usersCount.Set(float64(count))
+	// 		avgFollowersCount.Set(avgFollowers)
+	// 		time.Sleep(60 * time.Second)
+	// 	}
+	// }()
+
+	// app.Get("/metrics", iris.FromStd(promhttp.Handler()))
+
+	// // make sure the latest row is in the database
+	// db.FirstOrCreate(&models.Latest{
+	// 	// id is always 0
+	// 	ID: 0,
+	// })
+	// api := mvc.New(app.Party("/api"))
+	// api.Register(db)
+	// api.Register(timelineService)
+	// api.Register(messageService)
+	// api.Register(userService)
+	// api.Register(authService)
+	// api.Handle(new(controllers.ApiController))
+
+	signer := jwt.NewSigner(jwt.HS256, sigKey, 10*time.Minute)
+
+	verifier := jwt.NewVerifier(jwt.HS256, sigKey)
+
+	verifier.WithDefaultBlocklist()
+
+	authMiddleware := verifier.Verify(func() interface{} {
+		return new(UserClaims)
 	})
-	avgFollowersCount := promauto.NewGauge(prometheus.GaugeOpts{
-		Subsystem: "minitwit",
-		Name:      "average_followers_count",
-		Help:      "The total amount of users in the database",
-	})
-	//run non-middleware metrics data collection for in separate thread.
-	// middleware data is collected in ./middleware/prometheusMiddleware.go
-	go func() {
-		for {
-			db.Model(&models.User{}).Count(&count)
-			db.Raw("select ((select count(follower_id) from followers) / (select count(*) from users));").Scan(&avgFollowers)
-			usersCount.Set(float64(count))
-			avgFollowersCount.Set(avgFollowers)
-			time.Sleep(60 * time.Second)
+
+	app.Get("/api/tweets", indexHandler(db))
+
+	app.Post("/api/signup", signupHandler(db, userService, authService))
+
+	app.Post("/api/signin", signinHandler(signer, db))
+
+	// protectedAPI := app.Party("/api/protected")
+	// protectedAPI.Use(authMiddleware)
+	app.Get("/api/timeline", timeline(db)).Use(authMiddleware)
+
+	app.Listen(":8080")
+}
+
+func indexHandler(db *gorm.DB) iris.Handler {
+	return func(ctx iris.Context) {
+		tweets := []services.Tweet{}
+		err := db.Model(&models.User{}).Select("users.id as UserId", "users.Username", "users.Email", "messages.id", "messages.Author_id", "messages.Text", "messages.Pub_date", "messages.Flagged").Joins("INNER JOIN messages ON messages.author_id = users.id AND messages.flagged = 0").Order("messages.pub_date DESC").Limit(30).Scan(&tweets).Error
+
+		if err != nil {
+			// return nil, err
 		}
-	}()
 
-	app.Get("/metrics", iris.FromStd(promhttp.Handler()))
+		services.AddAvatarAndDates(&tweets)
 
-	// make sure the latest row is in the database
-	db.FirstOrCreate(&models.Latest{
-		// id is always 0
-		ID: 0,
-	})
-	api := mvc.New(app.Party("/api"))
-	api.Register(db)
-	api.Register(timelineService)
-	api.Register(messageService)
-	api.Register(userService)
-	api.Register(authService)
-	api.Handle(new(controllers.ApiController))
+		ctx.JSON(tweets)
+	}
+}
 
-	tweetsAPI := app.Party("/tweets")
-	{
-		tweetsAPI.Get("/", func(ctx iris.Context) {
-			tweets := []services.Tweet{}
-			err := db.Model(&models.User{}).Select("users.id as UserId", "users.Username", "users.Email", "messages.id", "messages.Author_id", "messages.Text", "messages.Pub_date", "messages.Flagged").Joins("INNER JOIN messages ON messages.author_id = users.id AND messages.flagged = 0").Order("messages.pub_date DESC").Limit(30).Scan(&tweets).Error
+func signupHandler(db *gorm.DB, userService *services.UserService, authService *services.AuthService) iris.Handler {
+	return func(ctx iris.Context) {
+		user := controllers.RegisterUser{}
+		err := ctx.ReadJSON(&user)
 
-			if err != nil {
-				// return nil, err
-			}
+		if err != nil {
+			ctx.StatusCode(422)
+			ctx.JSON(iris.Map{"error": err.Error()})
+			// ctx.JSON(err)
+			// ctx.WriteString(err.Error())
 
-			services.AddAvatarAndDates(&tweets)
+			return
+		}
 
-			ctx.JSON(tweets)
+		if user.Username == "" {
+			ctx.StatusCode(400)
+			ctx.JSON(iris.Map{"status": 400, "error_msg": "you have to enter a username"})
+
+			return
+		}
+
+		if user.Email == "" || !strings.Contains(user.Email, "@") {
+			ctx.StatusCode(400)
+			ctx.JSON(iris.Map{"status": 400, "error_msg": "you have to enter a valid email address"})
+
+			return
+		}
+
+		if user.Password == "" {
+			ctx.StatusCode(400)
+			ctx.JSON(iris.Map{"status": 400, "error_msg": "you have to enter a password"})
+
+			return
+		}
+
+		exists, _ := userService.CheckUsernameExists(user.Username)
+
+		if exists {
+			ctx.StatusCode(400)
+			ctx.JSON(iris.Map{"status": 400, "error_msg": "the username is already taken"})
+
+			return
+		}
+
+		err = authService.CreateUser(user.Username, user.Email, user.Password)
+
+		if err == nil {
+			// update_latest(c)
+			ctx.StatusCode(204)
+
+			return
+		}
+	}
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func signinHandler(signer *jwt.Signer, db *gorm.DB) iris.Handler {
+	return func(ctx iris.Context) {
+		// Sign in logic
+		loginRequest := LoginRequest{}
+		err := ctx.ReadJSON(&loginRequest)
+
+		if err != nil {
+			ctx.StatusCode(422)
+			ctx.JSON(iris.Map{"error": err.Error()})
+
+			return
+		}
+
+		user := &models.User{}
+		result := db.First(&user, "username = ?", loginRequest.Username)
+
+		if result.Error != nil {
+			ctx.StatusCode(400)
+			ctx.JSON(iris.Map{"errors": "Invalid username and/or password"})
+
+			return
+		}
+
+		passwordErr := bcrypt.CompareHashAndPassword([]byte(user.Pw_Hash), []byte(loginRequest.Password))
+
+		if passwordErr != nil {
+			ctx.StatusCode(400)
+			ctx.JSON(iris.Map{"errors": "Invalid username and/or password"})
+
+			return
+		}
+
+		claims := UserClaims{
+			Id:       user.ID,
+			Username: user.Username,
+		}
+
+		token, err := signer.Sign(claims)
+		if err != nil {
+			ctx.StopWithStatus(iris.StatusInternalServerError)
+			return
+		}
+
+		ctx.StatusCode(200)
+		ctx.JSON(iris.Map{
+			"access_token": string(token[:]),
+			"username":     user.Username,
 		})
 	}
+}
 
-	app.Listen(":8080", iris.WithOptimizations)
+func timeline(db *gorm.DB) iris.Handler {
+	return func(ctx iris.Context) {
+		claims := jwt.Get(ctx).(*UserClaims)
+
+		tweets := []services.Tweet{}
+		err := db.Raw(`
+			SELECT
+				users.id as UserId,
+				users.Username,
+				users.Email,
+				messages.id,
+				messages.Author_id,
+				messages.Text,
+				messages.Pub_date,
+				messages.Flagged
+			from users, messages
+			where
+				messages.flagged = 0 and
+				messages.author_id = users.id and
+				(
+					users.id = ? or
+					users.id in (select follower_id from followers where user_id = ?)
+				)
+			order by messages.pub_date DESC
+			limit ?
+		`, claims.Id, claims.Id, 30).Scan(&tweets).Error
+
+		if err != nil {
+
+		}
+
+		services.AddAvatarAndDates(&tweets)
+
+		ctx.JSON(tweets)
+	}
 }
