368637204865176c404f3af1b767de8deaf51252
askw <askw@itu.dk>
2022-04-10 15:20:05 +0200

    Update mysql.go
    Co-Authored-By: Alexander Wermuth <alexwer2@gmail.com>

--- server/database/mysql.go
+++ server/database/mysql.go
@@ -33,6 +33,14 @@ func ConnectMySql() (*gorm.DB, error) {
 	address := os.Getenv("MYSQL_ADDRESS")
 	port := os.Getenv("MYSQL_PORT")
 	db_name := os.Getenv("MYSQL_DATABASE")
+
+	// TODO REMOVE THIS
+	fmt.Println("Pass: " + password)
+	fmt.Println("user: " + user)
+	fmt.Println("address: " + address)
+	fmt.Println("db_name: " + db_name)
+	fmt.Println("port: " + port)
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, address, port, db_name)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
