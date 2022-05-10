69b3fcbf81d51e56564213cc37ca97367b70afd5
askw <askw@itu.dk>
2022-05-10 14:08:33 +0200

    pem keys
    Co-Authored-By: Alexander Wermuth <alexwer2@gmail.com>

--- terraform/genKeys.sh
+++ terraform/genKeys.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-mkdir ssh_key && ssh-keygen -t rsa -b 4096 -q -N '' -f ./ssh_key/terraform
+mkdir ssh_key #&& ssh-keygen -t rsa -b 4096 -q -N '' -f ./ssh_key/terraform
+
+ssh-keygen -t rsa -m PEM -f ./ssh_key/terraform
